588ebea0c117801367bc29254adee38ccd0e5f9e
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2023-05-29 10:07:50 +0530

    Time: 103 ms (76.51%), Space: 51.4 MB (88.34%) - LeetHub

---
+++ 2758-check-if-object-instance-of-class/2758-check-if-object-instance-of-class.js
@@ -0,0 +1,29 @@
+/**
+ * @param {any} object
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+
+const checkIfInstanceOf = (obj, classFunction) => {
+  
+  if (obj===null || obj===undefined || typeof classFunction !== 'function') {
+    return false; 
+  }
+
+  let prototype = Object.getPrototypeOf(obj); 
+
+  while (prototype) {
+
+    if (prototype === classFunction.prototype) {
+      
+      return true; 
+    }
+    prototype = Object.getPrototypeOf(prototype); 
+  }
+  return false; 
+};
+
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
